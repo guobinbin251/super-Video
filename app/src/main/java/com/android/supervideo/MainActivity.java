05a7277cc941562552861943df749c5047367867
Andy Guo <andy.guo@ixdigit.com>
2018-12-13 14:25:12 +0800

    新增一个签名文件 全屏 视频横屏播放

--- app/src/main/java/com/android/supervideo/MainActivity.java
+++ app/src/main/java/com/android/supervideo/MainActivity.java
@@ -97,6 +97,17 @@ public class MainActivity extends AppCompatActivity implements ITXLivePlayListen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        WindowManager.LayoutParams lp = this.getWindow().getAttributes();
+        lp.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        this.getWindow().setAttributes(lp);
+        /*View decorView = getWindow().getDecorView();
+        int systemUiVisibility = decorView.getSystemUiVisibility();
+        int flags = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        systemUiVisibility |= flags;
+        getWindow().getDecorView().setSystemUiVisibility(systemUiVisibility);*/
+
         setContentView(R.layout.activity_main);
         mPlayerView = findViewById(R.id.video_view);
 
